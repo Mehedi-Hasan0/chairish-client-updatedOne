c6bea290a4eff93e83a95fd19eee36389bf88119
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-12-25 17:53:56 +0600

    because of server issue last repo is not working. This is the new one

---
+++ src/Pages/Shop/Cart/Cart.js
@@ -0,0 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Loading from '../../Loading/Loading';
+import CartProductsCard from './CartProductsCard/CartProductsCard';
+
+const Cart = () => {
+    const [cartProducts, setCartProducts] = useState([]);
+    const { data: viewCart = [], isLoading } = useQuery({
+        queryKey: ['viewCart'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/viewCart');
+            const data = await res.json();
+            setCartProducts(data);
+            return data;
+        }
+    })
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    return (
+        <section>
+            <div className='max-w-6xl mx-auto lg:pb-40  py-20 px-5 sm:px-9 md:px-12 lg:px-16 xl:px-0'>
+                <div>
+                    <h3 className='text-dark text-3xl font-semibold'>Shopping Cart</h3>
+                    <p className=' text-sm mt-3'><Link to='/'>HomePage</Link> / <Link to='/shop'>Shop</Link> / <span className=' underline'>Shopping Cart</span></p>
+                    <hr className='my-10 border-primary' />
+                </div>
+                <div>
+                    <div className='w-[60%]'>
+                        {
+                            viewCart.map((products, i) => <CartProductsCard key={i} products={products} />)
+                        }
+                    </div>
+                    <div className='w-[40%]'>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Cart;
